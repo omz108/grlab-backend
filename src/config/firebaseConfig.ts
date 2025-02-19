1c9e365a711650054d8d0df5f6c6a2806c048093
OM <omjaiswal2552@gmail.com>
2025-02-19 12:42:36 +0530

    added firebase env

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,9 +2,19 @@ import admin from "firebase-admin";
 import { readFileSync } from "fs";
 import path from "path";
 
+// const serviceAccount = JSON.parse(
+//   readFileSync(path.join(__dirname, "../config/uparkom-grlab-firebase-adminsdk-fbsvc-d637029fb3.json"), "utf-8")
+// );
+
+const serviceAccountString = process.env.FIREBASE_CREDENTIALS;
+if (!serviceAccountString) {
+  throw new Error("Missing FIREBASE_CREDENTIALS env variable");
+}
+
 const serviceAccount = JSON.parse(
-  readFileSync(path.join(__dirname, "../config/uparkom-grlab-firebase-adminsdk-fbsvc-d637029fb3.json"), "utf-8")
+  Buffer.from(serviceAccountString, "base64").toString("utf-8")
 );
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "gs://uparkom-grlab.firebasestorage.app",
